85f0bf73b70b743a1935de4cb4add48b54267285
Tamas Ungi <ungi.tamas@gmail.com>
2019-01-31 00:07:01 -0500

    Fixing issues with running the model.

--- LiveFeedback/CentralLinePhantom1.py
+++ LiveFeedback/CentralLinePhantom1.py
@@ -13,11 +13,13 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import *
 from keras.utils import Sequence
+
+import cv2
 from pyIGTLink import pyIGTLink
 
 # Parameters
 
-image_size = 512
+image_size = 128
 
 # Check command line arguments
 
@@ -46,21 +48,20 @@ model.add(Dropout(.2))
 model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
 model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(.2))
 
-model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
-model.add(Dropout(.2))
+model.add(Dropout(.4))
 model.add(Dense(2, activation='sigmoid'))
 
-model.summary()
+model.load_weights(weights_file_name)
 
 print("Server starting...")
 client = pyIGTLink.PyIGTLinkClient(host="127.0.0.1")
 client.start()
 print("Server running...")
 try:
+    image_squeezed = np.zeros([image_size, image_size])
+    image_input = np.zeros([1, image_size, image_size, 1])
     while True:
       messages = client.get_latest_messages()
       if len(messages) > 0:
@@ -69,7 +70,9 @@ try:
             image = message._image
             #image = np.flip(image, 1)
             #image = np.flip(image, 2)
-            prediction = model.predict(image).tolist()
+            image_squeezed = np.squeeze(image)
+            image_input[0,:,:,0] = cv2.resize(image_squeezed, (image_size, image_size)).astype(np.uint8)
+            prediction = model.predict(image_input).tolist()
             print("Predicted center line: " + str(prediction[0]))
             client.send_message(pyIGTLink.StringMessage(str(prediction[0]), device_name=message._device_name+"Predicted"))
       time.sleep(0.1)
