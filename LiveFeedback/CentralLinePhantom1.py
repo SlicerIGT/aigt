d3f093990e53a31c32d010e9c51d9403f1b85721
Tamas Ungi <ungi.tamas@gmail.com>
2019-02-01 00:19:14 -0500

    More or less acceptable live feedback demo. Added image display to the live script for debugging image orientation.

--- LiveFeedback/CentralLinePhantom1.py
+++ LiveFeedback/CentralLinePhantom1.py
@@ -15,6 +15,7 @@ from keras.layers.convolutional import *
 from keras.utils import Sequence
 
 import cv2
+import matplotlib.pyplot as plt
 from pyIGTLink import pyIGTLink
 
 # Parameters
@@ -59,23 +60,35 @@ print("Server starting...")
 client = pyIGTLink.PyIGTLinkClient(host="127.0.0.1")
 client.start()
 print("Server running...")
+
 try:
     image_squeezed = np.zeros([image_size, image_size]).astype(np.uint8)
     image_padded = np.zeros([1, image_size, image_size, 1]).astype(np.uint8)
     image_input = np.zeros([1, image_size, image_size, 1]).astype(np.uint8)
+    
+    cv2.circle(image_input[0,:,:,0], (10,10), 15, 255, 5)
+    cv2.imshow("image", image_input[0,:,:,0])
+    cv2.waitKey(10)
+    
     while True:
       messages = client.get_latest_messages()
       if len(messages) > 0:
         for message in messages:
           if message._type == "IMAGE":
             image = message._image
-            #image = np.flip(image, 1)
-            #image = np.flip(image, 2)
+            image = np.flip(image, 1)
             image_squeezed = np.squeeze(image)
             image_padded[0,:,:,0] = cv2.resize(image_squeezed, (image_size, image_size)).astype(np.uint8)
             image_input = image_padded / 255.0
             prediction = model.predict(image_input).tolist()
+            
             print("Predicted center line: " + str(prediction[0]))
+            cx = int(image_size*prediction[0][0])
+            cy = int(image_size*prediction[0][1])
+            cv2.circle(image_input[0,:,:,0], (cx, cy), 2, 255, thickness=1)
+            cv2.imshow("image", image_input[0,:,:,0])
+            cv2.waitKey(10)
+            
             client.send_message(pyIGTLink.StringMessage(str(prediction[0]), device_name=message._device_name+"Predicted"))
       time.sleep(0.1)
 except KeyboardInterrupt:
