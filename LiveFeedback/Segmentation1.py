fa73fabd2407ceab4e6f9e66334fa52e5ea69306
Tamas Ungi <ungi@queensu.ca>
2019-03-11 11:24:27 -0400

    Fixed response image origin and orientation

--- LiveFeedback/Segmentation1.py
+++ LiveFeedback/Segmentation1.py
@@ -48,8 +48,8 @@ try:
     image_padded = np.zeros([1, image_size, image_size, 1]).astype(np.uint8)
     image_input = np.zeros([1, image_size, image_size, 1]).astype(np.uint8)
     
-    cv2.imshow("image", image_input[0,:,:,0])
-    cv2.waitKey(10)
+    # cv2.imshow("image", image_input[0,:,:,0])
+    # cv2.waitKey(10)
 
     while True:
       messages = client.get_latest_messages()
@@ -61,13 +61,15 @@ try:
             image_squeezed = np.squeeze(image)
             image_padded[0,:,:,0] = cv2.resize(image_squeezed, (unet_input_image_size, unet_input_image_size)).astype(np.uint8)
             image_input = image_padded / 255.0
-            prediction = model.predict(image_input) * 255
-            prediction_resized = np.zeros([1, 512, 512, 1])
-            prediction_resized[0, :, :, 0] = cv2.resize(prediction[0, :, :, 0], (512, 512)).astype(np.uint8)
+            prediction = np.flip(model.predict(image_input) * 255, 1)
+            prediction_resized = np.zeros(image.shape)
+            prediction_resized[0, :, :, 0] = cv2.resize(prediction[0, :, :, 0], (image.shape[1], image.shape[2])).astype(np.uint8)
             image_message = pyIGTLink.ImageMessage(prediction_resized, device_name=message._device_name + "Predicted")
+            image_message._matrix[0][3] = image.shape[1] / 2
+            image_message._matrix[1][3] = image.shape[2] / 2
             print(image_message._matrix)
             client.send_message(image_message)
-      time.sleep(0.1)
+      time.sleep(0.05)
 except KeyboardInterrupt:
     pass
 
