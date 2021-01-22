566982c82ee425f1afed90ce434e4f8467deaa0a
RebeccaHisey <11rjh10@queensu.ca>
2021-01-22 15:08:49 -0500

    train neural net now functional

---
+++ DeepLearnLive/TrainNeuralNet/Scripts/TrainNeuralNet.bat
@@ -0,0 +1,11 @@
+set CondaPath=%1
+Echo %CondaPath%
+set EnvName=%2
+Echo %EnvName%
+set DataCSV=%3
+set SaveLocation=%4
+set TrainingScript=%5
+
+call %CondaPath%\Scripts\activate %EnvName%
+call python %TrainingScript% --save_location=%SaveLocation% --data_csv_file=%DataCSV% 
+call %CondaPath%\Scripts\deactivate
