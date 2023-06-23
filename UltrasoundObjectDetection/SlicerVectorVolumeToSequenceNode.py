0f922d76126a011e3c5f581e90adef83f9854a20
Robert Zsolt Szabo <szabo.rozso@gmail.com>
2023-06-23 14:46:50 -0400

    OpenIGTLink test files

--- UltrasoundObjectDetection/SlicerVectorVolumeToSequenceNode.py
+++ UltrasoundObjectDetection/SlicerVectorVolumeToSequenceNode.py
@@ -1,19 +1,23 @@
-nodeName = "SCN_17_meroleges_LLA_433.dcm.dcm"
+import numpy as np
+
+nodeName = "SCN_22_meroleges_RUA_334"
 videoVolume = slicer.util.getNode(nodeName)
 videoArray = slicer.util.arrayFromVolume(videoVolume)
 index = 0.00
 sequenceNode = slicer.vtkMRMLSequenceNode()
 slicer.mrmlScene.AddNode(sequenceNode)
+sequenceNode.SetName("ImageReference")
 for i in range(videoArray.shape[0]):
     index += 0.1
     indexFloat = round(float(index), 2)
-    newVideoNode = slicer.vtkMRMLVectorVolumeNode()
+    newVideoNode = slicer.vtkMRMLScalarVolumeNode()
     newVideoNode.SetName(nodeName + "-Sequence_" + str(i).zfill(4))
 
     imageSpacing = [0.2, 0.2, 0.2]
     imageData = vtk.vtkImageData()
 
     imageMat = np.rot90(videoArray[i], 3)
+    imageMat = np.flip(imageMat, axis=0)
     destinationArray = vtk.util.numpy_support.numpy_to_vtk(imageMat.transpose(2, 1, 0).ravel(), deep=True)
     imageData.SetDimensions(imageMat.shape)
     imageData.GetPointData().SetScalars(destinationArray)
